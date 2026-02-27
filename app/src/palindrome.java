14acfec024f0651077ff1c3b4da540f262657030
Santhosh Karthikeyan <sasanthoshkarthikeyan@gmail.com>
2026-02-27 15:58:22 +0530

    SanthoshKarthikeyan

--- app/src/palindrome.java
+++ app/src/palindrome.java
@@ -1,6 +1,25 @@
-public class palindrome {
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
     public static void main(String[] args) {
-    System.out.println("Welcome to Palindrome Checker App management System");
-    System.out.println("Modify this logic to Usecase 2");
-}
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        scanner.close();
+    }
 }
