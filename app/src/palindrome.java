1abafab7c368b5d1a7124699cc5392f9dc100cd2
Santhosh Karthikeyan <sasanthoshkarthikeyan@gmail.com>
2026-02-27 15:23:52 +0530

    santhosh_karthikeyan

---
+++ app/src/palindrome.java
@@ -0,0 +1,6 @@
+public class palindrome {
+    public static void main(String[] args) {
+    System.out.println("Welcome to Palindrome Checker App management System");
+    System.out.println("Modify this logic to Usecase 2");
+}
+}
