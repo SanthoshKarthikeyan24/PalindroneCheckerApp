9aaeca4c596c40e9bbfea95baec6c68cb09d16d9
Santhosh Karthikeyan <sasanthoshkarthikeyan@gmail.com>
2026-02-27 16:15:11 +0530

    your_name Added UsecaseName

--- app/src/palindrome.java
+++ app/src/palindrome.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class PalindromeChecker {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String word = scanner.nextLine();
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
 
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        if (word.equalsIgnoreCase(reversed)) {
+        if (original.equalsIgnoreCase(reversed)) {
+
+
             System.out.println("It is a palindrome.");
+
         } else {
             System.out.println("It is not a palindrome.");
         }
